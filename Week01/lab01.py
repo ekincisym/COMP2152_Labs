389accb300583234551757af43e09a055fca2e7d
ekincisym <seymasibelaydin@gmail.com>
2026-01-24 16:00:43 -0500

    adding q1,q2,q3

---
+++ Week01/lab01.py
@@ -0,0 +1,7 @@
+# Sample Coding Questions 01 Week 01
+# Seyma Sibel Ekinci
+
+
+
+# Question 2
+my_array = [1,4,7,9]
---
+++ Week01/q3.py
@@ -0,0 +1,8 @@
+# Question 3: Order of Operations
+a = 1
+b = 2
+c = 3
+d = 4
+
+# Fully-Bracketed Version of: e = a- b ** c // d + a % c
+e = ( a - ((b ** c) // d)) + ( a % c)
