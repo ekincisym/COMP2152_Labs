bc6cfc201061bde42d64002520023901b5ee2a7d
ekincisym <seymasibelaydin@gmail.com>
2026-01-30 14:45:50 -0500

    add w03

---
+++ Week03/Q1.py
@@ -0,0 +1,10 @@
+grades = [85, 92, 78, 95, 88]
+grades.append(90)
+grades.sort()
+print(f"Sorted grades: {grades}") # Sorted grades: [78, 85, 88, 90, 92, 95]
+print(f"Highest grades: {grades[-1]}")
+print(f"Lowest grades: {grades[0]}")
+print(f"Total number of grades: {len(grades)}")
+
+
+
